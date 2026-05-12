7dd4426b0403d635090a0f63a542f8a4e2e0840c
huifangsu6-cyber <huifangsu6@gmail.com>
2026-05-12 19:44:49 +0100

    Update q4_feature_boost.sql

--- sql/q4_feature_boost.sql
+++ sql/q4_feature_boost.sql
@@ -1,1 +1,7 @@
+-- Q4: Single-feature boost analysis
+-- Purpose: Compare OutdoorSeating, Caters, and RestaurantsReservations
 
+SELECT
+  ...
+FROM `bdao-group-yelp.yelp_dataset.yelp_restaurants`
+...
